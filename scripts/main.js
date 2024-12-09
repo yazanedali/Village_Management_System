29f903f8240293b87510bb1fa6cb1b2747aa04b6
Deyaanoor <127193661+Deyaanoor@users.noreply.github.com>
2024-12-09 15:23:12 +0200

    12/9/2024

--- scripts/main.js
+++ scripts/main.js
@@ -67,4 +67,38 @@ links.forEach((link) => {
     const script = link.dataset.script;
     loadPage(page, script);
   });
+
+const username = localStorage.getItem('username');
+const typeUser = localStorage.getItem('userType'); 
+
+if (username) {
+  document.querySelector('#admin-info h4').innerText = username;
+
+  let imageUrl = '';
+  if (typeUser === "admin" && username === "deyaa") {
+    imageUrl = "https://th.bing.com/th/id/OIP.USP1T_5fjD1VcqeFBkbNDwHaHa?rs=1&pid=ImgDetMain&fbclid=IwZXh0bgNhZW0CMTEAAR0vJqEiR6evR_QCJCdRw4ypP4gCVb0VWgYTiF_7eJjsuIUTM9jaS0f4f8I_aem_Pm_DYSNedC84sovrP1OEgQ";
+  } else if (typeUser === "admin" && username === "yazan") {
+    console.log("first")
+    imageUrl = "https://th.bing.com/th/id/OIP.USP1T_5fjD1VcqeFBkbNDwHaHa?rs=1&pid=ImgDetMain&fbclid=IwZXh0bgNhZW0CMTEAAR0vJqEiR6evR_QCJCdRw4ypP4gCVb0VWgYTiF_7eJjsuIUTM9jaS0f4f8I_aem_Pm_DYSNedC84sovrP1OEgQ";
+  } else if (typeUser === "admin" && username === "moamen") {
+    imageUrl = "https://th.bing.com/th/id/OIP.USP1T_5fjD1VcqeFBkbNDwHaHa?rs=1&pid=ImgDetMain&fbclid=IwZXh0bgNhZW0CMTEAAR0vJqEiR6evR_QCJCdRw4ypP4gCVb0VWgYTiF_7eJjsuIUTM9jaS0f4f8I_aem_Pm_DYSNedC84sovrP1OEgQ";
+  }
+
+  if (imageUrl) {
+    document.querySelector('#admin-info img').src = imageUrl;
+  }
+} else {
+  window.location.href = 'login.html';
+}
+
+document.querySelector('a[href="#logout"]').addEventListener('click', function () {
+  localStorage.removeItem('username');
+  localStorage.removeItem('typeUser'); 
+  
+  window.location.href = 'login.html'; 
+});
+
+
+
+  
 });
