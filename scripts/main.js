32df4496d0d17aa1338525cd65c61ce8f86ab1dc
yazanedali <edaliyazan6@gmail.com>
2024-12-10 20:49:21 +0200

    maps

--- scripts/main.js
+++ scripts/main.js
@@ -27,6 +27,11 @@ const loadPage = (page, script) => {
       if (script) {
         loadScript(script);
       }
+
+      if (page === "overview.html") {
+        loadGoogleMapsAPI(); 
+      }
+
     })
     .catch((error) => {
       console.error("Error loading the page:", error);
@@ -34,6 +39,33 @@ const loadPage = (page, script) => {
     });
 };
 
+
+const loadGoogleMapsAPI = () => {
+  if (!document.querySelector('script[src*="maps.googleapis.com"]')) {
+    const script = document.createElement("script");
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAysQBM3-leTK8l9EKH9GpzS5HeIplJeUM&callback=initMap";
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+  }
+   else {
+    initMap();
+  }
+};
+
+function initMap() {
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 31.9686, lng: 35.2433 },
+    zoom: 8,
+  });
+
+  const marker = new google.maps.Marker({
+    position: { lat: 31.4086, lng: 34.3433 },
+    map: map,
+    title: "Main Location",
+  });
+}
 
 const loadScript = (src) => {
 
@@ -97,8 +129,7 @@ document.querySelector('a[href="#logout"]').addEventListener('click', function (
   
   window.location.href = 'login.html'; 
 });
-
-
-
-  
 });
+
+
+
